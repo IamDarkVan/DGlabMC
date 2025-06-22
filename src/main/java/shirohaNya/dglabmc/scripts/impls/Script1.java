5c5a0d5276e0729f4b8da58557f5d5102f6285ad
ShirohaNya <iamdarkvannmsl@gmail.com>
2025-06-23 05:56:34 +0800

    Updated Generating QR code

--- src/main/java/shirohaNya/dglabmc/scripts/impls/Script1.java
+++ src/main/java/shirohaNya/dglabmc/scripts/impls/Script1.java
@@ -1,13 +1,11 @@
 package shirohaNya.dglabmc.scripts.impls;
 
-import shirohaNya.dglabmc.Client;
-import shirohaNya.dglabmc.scripts.Script;
-import shirohaNya.dglabmc.scripts.ScriptAbstract;
 import lombok.Getter;
+import shirohaNya.dglabmc.Client;
+import shirohaNya.dglabmc.scripts.ScriptAbstract;
 
 @Getter
 public class Script1 extends ScriptAbstract {
-    private final Script script1 = new Script1();
     public Script1() {
         // 电击时间: 5秒, 受伤后重置电击时间(否则累加): true
         super("script1", "受伤电人", null);
